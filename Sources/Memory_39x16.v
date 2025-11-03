4cf2b78648c809cefa6a4be3a6e19f30e7c0c83a
Keshav Baldeva <152970391+KeshavBaldeva@users.noreply.github.com>
2025-11-03 16:39:32 +0530

    Update Memory_39x16.v

--- Sources/Memory_39x16.v
+++ Sources/Memory_39x16.v
@@ -20,27 +20,29 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-// Raw memory storing 39-bit words (32 data + 7 ECC)
+// 39 bit is width for both data and parity bits and 16 is the depth
 module Memory_39x16 (
-  input  wire        clk,
+  input  wire clk,
 
   // Write
-  input  wire        wr_en,
-  input  wire [3:0]  wr_addr,
-  input  wire [38:0] wr_word,
+  input  wire wr_en,
+  input  wire [3:0]wr_addr,
+  input  wire [38:0]wr_word,
 
   // Read
-  input  wire        rd_en,
-  input  wire [3:0]  rd_addr,
-  output reg  [38:0] rd_word
+  input  wire rd_en,
+  input  wire [3:0]rd_addr,
+  output reg [38:0]rd_word
 );
-  reg [38:0] mem [0:15];
+  reg [38:0]mem [0:15];
 
-  always @(posedge clk) begin
+  always @(posedge clk) 
+   begin
     if (wr_en)
       mem[wr_addr] <= wr_word;
     if (rd_en)
       rd_word <= mem[rd_addr];
-  end
+   end
+  
 endmodule
 
